386968b01a7f49afc3a7252f2797ef3752965952
Bruno Notebook <fbrunmarcos@gmail.com>
2023-02-27 02:02:33 -0300

    realizado

--- scripts/main.js
+++ scripts/main.js
@@ -3,25 +3,22 @@ contenedorCard.innerHTML= Eventos()
 function Eventos(){
     let cards= ''
     for (const evento of data.events) {
-    cards+=` <div class='d-flex h-100 card col-xxl-3 col-xl-3 col-lg-4 col-sm-8 col-md-10'>
-    <div class='bg-success-subtle'>
-        <img src='${evento.image}' class='card-img-top' alt='...'>
-        <div class='card-body'>
-            <h5 class='card-title text-center fs-3'>${evento.name}</h5>
-            <p class='card-text fs-5'>${evento.description}</p>
-            <div class='prices fs-5'>
-                <label>Price: ${evento.price}</label>
-                <label>${evento.place}</label>
-                <label>${evento.category}</label>
-            </div>
-        </div>
-        <div class="card-footer">
-        <label class="text-muted bg-warning fs-6">Capacity:${evento.capacity}</label>
-        <label class="text-muted bg-warning fs-6">Date:${evento.date}</label>
-        <label class="text-muted bg-warning fs-6">Assistance:${evento.assistance}</label>
-        </div>
+    cards+=` <div class='card' style='width: 18rem;'>
+    <img src='${evento.image}' class='card-img-top' alt='...'>
+    <div class='card-body bg-primary'>
+      <h5 class='card-title fs-4 text-center'>${evento.name}</h5>
+      <p class='card-text fs-5'>${evento.description}</p>
     </div>
-</div>`                        
+    <ul class='list-group list-group-flush'>
+      <li class='list-group-item bg-info'>Category: ${evento.category}</li>
+      <li class='list-group-item bg-info'>Place: ${evento.place}</li>
+      <li class='list-group-item bg-info'>Price: ${evento.price}</li>
+    </ul>
+    <div class='card-footer bg-info'>
+      <a href'"#' class='card-link'>Capacity:${evento.capacity}</a>
+      <a href'"#' class='card-link d-flex flex-wrap m-0'>Assistance: ${evento.assistance}</a>
+    </div>
+  </div>`                        
 }
     return cards
 }
---
+++ scripts/past.js
@@ -0,0 +1,26 @@
+const UpcomingCards=document.querySelector("#cards2")
+UpcomingCards.innerHTML= UpcomingEvents()
+function UpcomingEvents(){
+        let cards= ''
+        for (const evento of data.events) {
+            if (evento.date < data.currentDate) {
+                cards+=` <div class='card' style='width: 18rem;'>
+                <img src='${evento.image}' class='card-img-top' alt='...'>
+                <div class='card-body bg-primary'>
+                  <h5 class='card-title fs-4 text-center'>${evento.name}</h5>
+                  <p class='card-text fs-5'>${evento.description}</p>
+                </div>
+                <ul class='list-group list-group-flush'>
+                  <li class='list-group-item bg-info'>Category: ${evento.category}</li>
+                  <li class='list-group-item bg-info'>Place: ${evento.place}</li>
+                  <li class='list-group-item bg-info'>Price: ${evento.price}</li>
+                </ul>
+                <div class='card-footer bg-info'>
+                  <a href'"#' class='card-link'>Capacity:${evento.capacity}</a>
+                  <a href'"#' class='card-link d-flex flex-wrap m-0'>Assistance: ${evento.assistance}</a>
+                </div>
+              </div>` 
+            }               
+    }
+        return cards
+}
