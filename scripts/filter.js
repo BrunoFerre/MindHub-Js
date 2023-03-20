c26165b9c0bd234a4570ed19da5d6ad32b668dbd
Bruno Ferreira <fbrunomarcos@gmail.com>
2023-03-20 02:05:42 -0300

    realizado

--- scripts/filter.js
+++ scripts/filter.js
@@ -1,17 +1,58 @@
+//capturar elemento
+let clase=document.querySelector(".category")
+let inputs=''
+const contenedorCard = document.querySelector("#cards2")
+clase.innerHTML=crearInputs()
+function crearInputs() {
+  let opciones=[]
+  for (const categorias of data.events) {
+    if (opciones.indexOf(categorias.category)=== -1) {
+      opciones.push(categorias.category)
+      inputs+=`<label for="${categorias.name}"><input class="form-check-input" name="status" type="checkbox" onchange="FiltrarCardCheckBox()" value="${categorias.category}" id="${categorias.name}">${categorias.category}</label>`
+    }
+  }
+  return inputs
+}
+function FiltrarCard(array) {
+  const buscador = document.getElementById("search")
+    contenedorCard.innerHTML=''
+     array = data.events.filter((categoria)=>categoria.category.toLowerCase().includes(buscador.value.toLowerCase()))
+     if (array == ""){
+       contenedorCard.innerHTML+=` <div class='card col-12' style='width: 18rem;'>
+      <img src='./assets/error.png' class='card-img-top' alt='...'>
+      <div class='card-body bg-primary'>
+        <h5 class='card-title fs-4 text-center'>It was not found</h5>
+      </div>
+      <div class='card-footer bg-info'>
+      <a href='./index.html' class="btn btn-primary">GO HOME</a>
+      </div>
+    </div>`
+     }else{
+     for (filtro of array) {
+        contenedorCard.innerHTML+=` <div class='card' style='width: 18rem;'>
+        <img src='${filtro.image}' class='card-img-top' alt='...'>
+        <div class='card-body bg-primary'>
+          <h5 class='card-title fs-4 text-center'>${filtro.name}</h5>
+        </div>
+        <ul class='list-group list-group-flush'>
+          <li class='list-group-item bg-info'>Category: ${filtro.category}</li>
+          <li class='list-group-item bg-info'>Place: ${filtro.place}</li>
+          <li class='list-group-item bg-info'>Price: ${filtro.price}</li>
+        </ul>
+        <div class='card-footer bg-info'>
+        <a href='./details.html?id=${filtro._id}' class="btn btn-primary">Ver mas</a>
+        </div>
+      </div>`
+  }
+}
+}
 
 function FiltrarCardCheckBox() {
   contenedorCard.innerHTML=''
   //capturar checkboxs
-  let checkbox = document.querySelectorAll('input[name="status"]:checked');
-  console.log(checkbox);
-  //array con checkboxs seleccionados
-  
-  let categoriasFiltradas =[]
-  checkbox.forEach(function (inputs) {
-    categoriasFiltradas.push(inputs.value)
-  })
-  array = data.events.filter((categoria)=>categoria.category.includes(categoriasFiltradas))
-  console.log(array);
+  let checkbox = document.querySelectorAll('input:checked');
+  let catString = Array.from(checkbox).map(c=>c.value)
+  array = data.events.filter((categoria)=>categoria.category.includes(catString))
   for ( categorias of array) {
     contenedorCard.innerHTML+=` <div class='card' style='width: 28rem;'>
     <img src='${categorias.image}' class='card-img-top' alt='...'>
--- scripts/stats.js
+++ scripts/stats.js
@@ -21,3 +21,133 @@ function pintartabla(array,lugar){
 })
     lugar.innerHTML=d
 }
+const primeraTabla = document.getElementById('capacity-statics')
+const segundaTabla = document.getElementById('UpcomingEvents')
+const terceraTabla = document.getElementById('PastEvents')
+function TraerDatos() {
+  fetch('./scripts/data.json')
+    .then(response => response.json())
+    .then(datosApis => {
+      const date = datosApis.currentDate
+      const upcomingEvents = []
+      const pastEvents = []
+      const opciones = []
+
+      //tipos de eventos 
+      for (const evento of datosApis.events) {
+        if (evento.date >= date) {
+          upcomingEvents.push(evento)
+        }else{
+          pastEvents.push(evento)
+        }
+      }
+       //generar categorias
+       for (const element of datosApis.events) {
+        if (opciones.indexOf(element.category) === -1) {
+          opciones.push(element.category);
+        }
+      }
+
+      //orden por asistencia y capacidad
+      //orden desendente
+      const EventosAsistenciaDesc = datosApis.events.map
+        ((evento) => ({
+          name: evento.name,
+          attendance:
+            evento.assistance === undefined ? (evento.estimate * 100) / evento.capacity
+              : (evento.assistance * 100) / evento.capacity,
+        })).sort((a, b) => b.attendance - a.attendance)
+      //orden ascendente
+      const EventosAsistenciaAsce = EventosAsistenciaDesc.slice().reverse()
+
+      //orden capacidad desc
+      const EventosCapacidadDesc = EventosAsistenciaDesc.slice().sort((a, b) => b.capacity - a.capacity)
+      //capturar primera tabla
+    
+      primeraTabla.innerHTML = PintarTabla(EventosAsistenciaDesc, EventosAsistenciaAsce, EventosCapacidadDesc)
+
+      function PintarTabla(EventosAsistenciaDesc, EventosAsistenciaAsce, EventosCapacidadDesc) {
+        let tabla = ''
+        let contador = 0
+        for (const [index, event] of EventosAsistenciaDesc.entries()) {
+          if (contador < 7) {
+            tabla += `<tr>
+          <td>${event.name}, ${event.attendance.toFixed(0)}%</td>
+          <td>${EventosAsistenciaAsce[index].name
+              }, ${EventosAsistenciaAsce[index].attendance.toFixed(0)}%</td>
+          <td>${EventosCapacidadDesc[index].name +":"+ EventosCapacidadDesc[index].attendance}</td>
+        </tr>`;
+          }
+          contador++
+          console.log(EventosAsistenciaDesc);
+        }
+        return tabla
+      }
+
+
+      segundaTabla.innerHTML = CreateUpcoming(upcomingEvents,opciones)
+
+
+      function CreateUpcoming(upcomingEvents, opciones) {
+        let tabla = ""
+        for (let i = 0; i < opciones.length; i++) {
+          let categorias2 = opciones[i]
+          let revenue = 0
+          let capacity = 0
+          let assistance = 0
+          for (const events of upcomingEvents) {
+            if (categorias2 == events.category) {
+              if (events.assistance === undefined) {
+                revenue = revenue + events.price * events.estimate
+                capacity = capacity + events.capacity
+                assistance = assistance + events.estimate
+              } else {
+                revenue = revenue + events.price * events.assistance;
+                capacity = capacity + events.capacity;
+                assistance = assistance + events.assistance;
+              }
+            }
+          }
+          tabla += `<tr>
+          <td>${categorias2}</td>
+          <td>$${revenue.toFixed(0)}</td>
+          <td>${((assistance * 100) / capacity).toFixed(0)}%</td>
+          </tr>`;
+        }
+        return tabla
+      }
+      terceraTabla.innerHTML=CreatePast(pastEvents,opciones)
+      function  CreatePast(pastEvents, opciones) {
+        let tabla = "";
+        for (let i = 0; i < opciones.length; i++) {
+          let categorias2 = opciones[i];
+          let revenue = 0;
+          let capacity = 0;
+          let assistance = 0;
+          for (const event of pastEvents) {
+            if (categorias2 == event.category) {
+              if (event.assistance === undefined) {
+                revenue = revenue + event.price * event.estimate;
+                capacity = capacity + event.capacity;
+                assistance = assistance + event.estimate;
+              } else {
+                revenue = revenue + event.price * event.assistance;
+                capacity = capacity + event.capacity;
+                assistance = assistance + event.assistance;
+              }
+            }
+          }
+          tabla += `<tr>
+                    <td>${categorias2}</td>
+                    <td>$${revenue.toFixed(0)}</td>
+                    <td>${((assistance * 100) / capacity).toFixed(0)}%</td>
+                  </tr>`;
+        }
+        return tabla;
+      }
+
+    })
+    .catch(error => console.log(error.message))
+}
+TraerDatos()
+
